cca5f27ed66b3af10643af466ee85c07b9ea141c
mashhoor-timber <mashhoor@timber.me>
2025-07-04 14:33:34 +0530

    feat(TIM-214): dropdown component

--- src/components/atomic/Dropdown/index.tsx
+++ src/components/atomic/Dropdown/index.tsx
@@ -1,54 +1,61 @@
+import React from "react";
 import {
   DropdownTrigger,
   extendVariants,
   Dropdown as NextDropdown,
   DropdownItem as NextDropdownItem,
   DropdownMenu as NextDropdownMenu,
-  DropdownMenuProps as NextDropdownMenuProps,
-  DropdownProps as NextDropdownProps,
-  DropdownItemProps as NextDropdownItemProps,
 } from "@heroui/react";
-import { ComponentType } from "react";
 
-const Dropdown = extendVariants(NextDropdown, {
+const Dropdown: React.ComponentType<any> = extendVariants(NextDropdown, {
   variants: {
     color: {
       default: {
-        base: "p-0 rounded-2xl min-w-[150px]",
+        content: "p-0 rounded-2xl min-w-[150px]",
       },
     },
   },
   defaultVariants: {
     color: "default",
   },
-}) as ComponentType<NextDropdownProps>;
+});
 
-const DropdownMenu = extendVariants(NextDropdownMenu, {
-  variants: {
-    color: {
-      default: {
-        base: "p-0",
-        list: "gap-0 *:rounded-none rounded-2xl *:px-4 *:py-3 *:border-b overflow-hidden",
+const DropdownMenu: React.ComponentType<any> = extendVariants(
+  NextDropdownMenu,
+  {
+    variants: {
+      color: {
+        default: {
+          base: "p-0",
+          list: "gap-0 *:rounded-none rounded-2xl pt-3 overflow-hidden",
+        },
+        text: {
+          base: "p-0",
+          list: "gap-0 *:rounded-none rounded-2xl *:px-4 *:py-3 *:border-b overflow-hidden",
+        },
       },
     },
-  },
-  defaultVariants: {
-    color: "default",
-  },
-}) as ComponentType<NextDropdownMenuProps>;
+    defaultVariants: {
+      color: "text",
+    },
+  }
+);
 
-const DropdownItem = extendVariants(NextDropdownItem, {
-  variants: {
-    color: {
-      default: {
-        base: "p-4 rounded-none border-b border-divider",
+const DropdownItem: React.ComponentType<any> = extendVariants(
+  NextDropdownItem,
+  {
+    variants: {
+      color: {
+        default: {
+          base: "p-4 rounded-none border-b border-divider",
+        },
       },
     },
-  },
-  defaultVariants: {
-    color: "default",
-  },
-}) as ComponentType<NextDropdownItemProps>;
+    defaultVariants: {
+      color: "default",
+    },
+  }
+);
 
 export default Dropdown;
 
